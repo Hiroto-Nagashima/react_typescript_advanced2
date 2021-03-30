717c954f744c93ef7bf9bda00fcb54f0a381ee2d
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-03-30 22:59:09 +0900

    finish practice

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,19 @@ import axios from "axios";
 import { useState } from "react";
 import "./styles.css";
 import { Todo } from "./Todo";
-type TodoType = {
-  userId: number;
-  id: number;
-  title: string;
-  completed: boolean;
-};
+import { Text } from "./Text";
+import { TodoType } from "./types/todo";
+import { UserProfile } from "./UserProfile";
 export default function App() {
+  const user = {
+    name: "Hiroto",
+    hobbies: ["野球", "ゲーム"]
+  };
+  // ステートの型定義　todos
   const [todos, setTodos] = useState<Array<TodoType>>([]);
   const onClickFetchData = () => {
     axios
+      // 取得するデータの型定義
       .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
       .then((res) => {
         setTodos(res.data);
@@ -19,9 +22,16 @@ export default function App() {
   };
   return (
     <div className="App">
+      <UserProfile user={user} />
+      <Text color={"red"} fontSize={"30px"} />
       <button onClick={onClickFetchData}>User</button>
       {todos.map((todo) => (
-        <Todo userid={todo.userId} title={todo.title} />
+        <Todo
+          key={todo.id}
+          userId={todo.userId}
+          title={todo.title}
+          completed={todo.completed}
+        />
       ))}
     </div>
   );
--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,4 +1,10 @@
-export const Todo = (props) => {
-  const { title, userid } = props;
-  return <p>{`${title}(ユーザー:${userid})`}</p>;
+import { VFC } from "react";
+import { TodoType } from "./types/todo";
+
+// export const Todo = (props:Pick<TodoType, "userId" | "completed" | "title">) => {
+export const Todo: VFC<Omit<TodoType, "id">> = (props) => {
+  // completedがundifiendだった時のための初期値設定
+  const { title, userId, completed = false } = props;
+  const completeMark = completed ? "[完]" : "[未]";
+  return <p>{`${completeMark}${title}(ユーザー:${userId})`}</p>;
 };
